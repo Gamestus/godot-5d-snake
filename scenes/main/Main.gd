5456670649e4036a9b43b008d9366766a114c1db
Gamestus <60428049+Gamestus@users.noreply.github.com>
2023-05-20 15:58:50 +0300

    Changed funcs positions

--- scenes/main/Main.gd
+++ scenes/main/Main.gd
@@ -1,7 +1,6 @@
 class_name Main
 extends Node2D
 
-var field = preload("res://scenes/main/Field.tscn")
 onready var map: Node2D = $Map
 onready var camera_2d: Camera2D = $Camera2D
 
@@ -13,6 +12,16 @@ const FIELD_OFFSET = Vector2(5, 5)
 const MIN_TIME_SCALE = 0.1
 const DELTA_TIME_SCALE = 0.15
 const MAX_TIME_SCALE = 2.1
+
+
+enum Cell{
+	NOTHING,
+	APPLE,
+	BODY,
+	HEAD,
+}
+
+var field = preload("res://scenes/main/Field.tscn")
 
 # Snake can move in 17 directions, so
 # direction_additional is essential
@@ -26,19 +35,12 @@ var score = 0
 var shader_time_param = 0.0
 var time_scale = 0.0
 var is_end_of_time = false
-
+var head_on_field: Field
 
 var snake_array = [
 		[Vector2(1, 5), Vector2(0, 0)],
 		[Vector2(0, 5), Vector2(0, 0)]
 ]
-
-enum Cell{
-	NOTHING,
-	APPLE,
-	BODY,
-	HEAD,
-}
 
 var field_dict = {
 	-4: [],
@@ -52,13 +54,6 @@ var field_dict = {
 	4: []
 }
 
-var head_on_field: Field
-
-func _process(delta: float) -> void:
-	shader_time_param += 0.01 * time_scale
-	$Camera2D/DarkMatter.material.set_shader_param("time", shader_time_param) 
-
-
 
 func _ready() -> void:
 	randomize()
@@ -71,6 +66,65 @@ func _ready() -> void:
 	
 	if TurboMode.turbo_mode:
 		$Ui/MarginContainer/VBoxContainer/HBoxContainer/StrangeLabel.visible = true
+
+
+func _process(delta: float) -> void:
+	shader_time_param += 0.01 * time_scale
+	$Camera2D/DarkMatter.material.set_shader_param("time", shader_time_param) 
+
+
+func _input(event: InputEvent) -> void:
+	if not TurboMode.turbo_mode and event.is_action_pressed("turbo"):
+		TurboMode.turbo_mode = true
+		$Ui/MarginContainer/VBoxContainer/HBoxContainer/StrangeLabel.visible = true
+		$APPLESound.play()
+	
+	var new_dir = Vector2.ZERO
+	var new_dir_a = Vector2.ZERO
+	if event.is_action_pressed("ui_up"):
+		new_dir = Vector2.UP
+	elif event.is_action_pressed("ui_down"):
+		new_dir = Vector2.DOWN
+	elif event.is_action_pressed("ui_left"):
+		new_dir = Vector2.LEFT
+	elif event.is_action_pressed("ui_right"):
+		new_dir = Vector2.RIGHT
+	elif event.is_action_pressed("ui_page_up"):
+		new_dir_a = Vector2.UP
+	elif event.is_action_pressed("ui_page_down"):
+		new_dir_a = Vector2.DOWN
+	elif event.is_action_pressed("forward"):
+		new_dir_a = Vector2.ZERO
+	elif event.is_action_pressed("past"):
+		new_dir_a = Vector2.LEFT
+	if new_dir != Vector2.ZERO and new_dir + last_direction != Vector2.ZERO:
+		direction = new_dir
+		direction_additional = Vector2.ZERO
+	elif new_dir_a != Vector2.ZERO and TurboMode.turbo_mode:
+		direction_additional = new_dir_a
+		direction = Vector2.ZERO
+	
+	$Ui/MarginContainer/VBoxContainer/HBoxContainer/StrangeLabel.text = str(direction) + " " + str(direction_additional)
+	
+	if event.is_action_pressed("speedup"):
+		$StepTimer.wait_time = clamp($StepTimer.wait_time - DELTA_TIME_SCALE, 
+				MIN_TIME_SCALE, 
+				MAX_TIME_SCALE
+		)
+	elif event.is_action_pressed("speeddown"):
+		$StepTimer.wait_time = clamp($StepTimer.wait_time + DELTA_TIME_SCALE, 
+				MIN_TIME_SCALE, 
+				MAX_TIME_SCALE
+		)
+
+
+func _on_StepTimer_timeout() -> void:
+	make_turn()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart"):
+		get_tree().reload_current_scene()
 
 
 func make_turn() -> void:
@@ -92,34 +146,6 @@ func make_turn() -> void:
 	
 	$Music.volume_db = linear2db(clamp(float(50 - snake_pos_a.x) / 50, 0, 0.4))
 	$Music2.volume_db = linear2db(clamp(float(snake_pos_a.x) / 50, 0, 0.5 ))
-
-
-func move_head_a(snake_pos, snake_pos_a):
-	$Camera2D.reveal()
-	is_end_of_time = true
-	snake_array.push_front(
-			[
-				Vector2(snake_pos.x + direction.x, snake_pos.y + direction.y),
-				Vector2(snake_pos_a.x + direction_additional.x, snake_pos_a.y + direction_additional.y),
-			]
-	)
-	
-	if not try_move(snake_array[0][0], snake_array[0][1]):
-		return
-	
-	head_on_field = field_dict[int(snake_array[0][1].y)][snake_array[0][1].x]
-	head_on_field.game_array[snake_array[0][0].x][snake_array[0][0].y] = Cell.HEAD
-	
-	if not is_making_longer:
-		var erase_cell = snake_array.pop_back()
-		var erase_field = field_dict[int(erase_cell[1].y)][erase_cell[1].x]
-		erase_field.game_array[erase_cell[0].x][erase_cell[0].y] = Cell.NOTHING
-		erase_field.draw_screen(direction)
-	is_making_longer = false
-	
-	head_on_field.draw_screen(direction)
-	last_direction = direction
-	camera_2d.tween_to(head_on_field.rect_position + Field.FIELD_SIZE / 2, false)
 
 
 func move_head(snake_pos, snake_pos_a):
@@ -152,6 +178,34 @@ func move_head(snake_pos, snake_pos_a):
 	
 	head_on_field.draw_screen(direction)
 	last_direction = direction
+
+
+func move_head_a(snake_pos, snake_pos_a):
+	$Camera2D.reveal()
+	is_end_of_time = true
+	snake_array.push_front(
+			[
+				Vector2(snake_pos.x + direction.x, snake_pos.y + direction.y),
+				Vector2(snake_pos_a.x + direction_additional.x, snake_pos_a.y + direction_additional.y),
+			]
+	)
+	
+	if not try_move(snake_array[0][0], snake_array[0][1]):
+		return
+	
+	head_on_field = field_dict[int(snake_array[0][1].y)][snake_array[0][1].x]
+	head_on_field.game_array[snake_array[0][0].x][snake_array[0][0].y] = Cell.HEAD
+	
+	if not is_making_longer:
+		var erase_cell = snake_array.pop_back()
+		var erase_field = field_dict[int(erase_cell[1].y)][erase_cell[1].x]
+		erase_field.game_array[erase_cell[0].x][erase_cell[0].y] = Cell.NOTHING
+		erase_field.draw_screen(direction)
+	is_making_longer = false
+	
+	head_on_field.draw_screen(direction)
+	last_direction = direction
+	camera_2d.tween_to(head_on_field.rect_position + Field.FIELD_SIZE / 2, false)
 
 
 func move_head_from_past(snake_pos, snake_pos_a):
@@ -211,51 +265,6 @@ func create_global_field(additional_pos):
 	return new_field
 
 
-func _input(event: InputEvent) -> void:
-	if not TurboMode.turbo_mode and event.is_action_pressed("turbo"):
-		TurboMode.turbo_mode = true
-		$Ui/MarginContainer/VBoxContainer/HBoxContainer/StrangeLabel.visible = true
-		$APPLESound.play()
-	
-	var new_dir = Vector2.ZERO
-	var new_dir_a = Vector2.ZERO
-	if event.is_action_pressed("ui_up"):
-		new_dir = Vector2.UP
-	elif event.is_action_pressed("ui_down"):
-		new_dir = Vector2.DOWN
-	elif event.is_action_pressed("ui_left"):
-		new_dir = Vector2.LEFT
-	elif event.is_action_pressed("ui_right"):
-		new_dir = Vector2.RIGHT
-	elif event.is_action_pressed("ui_page_up"):
-		new_dir_a = Vector2.UP
-	elif event.is_action_pressed("ui_page_down"):
-		new_dir_a = Vector2.DOWN
-	elif event.is_action_pressed("forward"):
-		new_dir_a = Vector2.ZERO
-	elif event.is_action_pressed("past"):
-		new_dir_a = Vector2.LEFT
-	if new_dir != Vector2.ZERO and new_dir + last_direction != Vector2.ZERO:
-		direction = new_dir
-		direction_additional = Vector2.ZERO
-	elif new_dir_a != Vector2.ZERO and TurboMode.turbo_mode:
-		direction_additional = new_dir_a
-		direction = Vector2.ZERO
-	
-	$Ui/MarginContainer/VBoxContainer/HBoxContainer/StrangeLabel.text = str(direction) + " " + str(direction_additional)
-	
-	if event.is_action_pressed("speedup"):
-		$StepTimer.wait_time = clamp($StepTimer.wait_time - DELTA_TIME_SCALE, 
-				MIN_TIME_SCALE, 
-				MAX_TIME_SCALE
-		)
-	elif event.is_action_pressed("speeddown"):
-		$StepTimer.wait_time = clamp($StepTimer.wait_time + DELTA_TIME_SCALE, 
-				MIN_TIME_SCALE, 
-				MAX_TIME_SCALE
-		)
-
-
 func is_field_exists(move_to_a):
 	return move_to_a.x >= 0 and move_to_a.x < APOCALYPSE_FIELD and\
 			move_to_a.y >= -MAX_DIMENSION_FIELD and move_to_a.y <= MAX_DIMENSION_FIELD
@@ -305,15 +314,6 @@ func create_field_at_dimension(move_to_a: Vector2):
 	create_field(new_y, Vector2(0, 60 * direction_additional.y))
 
 
-func _on_StepTimer_timeout() -> void:
-	make_turn()
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("restart"):
-		get_tree().reload_current_scene()
-
-
 func game_over() -> void:
 	$GameOverSound.play()
 	$StepTimer.stop()
@@ -325,3 +325,4 @@ func game_over() -> void:
 		$Camera2D.show_progress()
 	else:
 		$Ui/GameOver/AnimationPlayer.play("KO")
+
